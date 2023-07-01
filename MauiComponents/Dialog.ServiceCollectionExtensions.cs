323779b282d824b85c99bedd4de7bb7d8155ee9c
usausa <machi.pon@gmail.com>
2023-07-01 16:52:32 +0900

    Update dialog

--- MauiComponents/Dialog.ServiceCollectionExtensions.cs
+++ MauiComponents/Dialog.ServiceCollectionExtensions.cs
@@ -10,10 +10,14 @@ public static partial class ServiceCollectionExtensions
     public static IServiceCollection AddComponentsDialog(this IServiceCollection service, Action<DialogOptions> configure)
     {
 #if ANDROID
-        var options = new DialogOptions();
-        configure(options);
 
-        service.AddSingleton<IDialog>(new DialogImplementation(options));
+        service.AddSingleton<DialogOptions>(_ =>
+        {
+            var options = new DialogOptions();
+            configure(options);
+            return options;
+        });
+        service.AddSingleton<IDialog, DialogImplementation>();
 #endif
         return service;
     }
