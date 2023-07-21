9cc0b684c8e9aed1a07012415d9fa933d24165d4
usausa <machi.pon@gmail.com>
2023-07-21 10:30:41 +0900

    Update

--- MauiComponents/Dialog.cs
+++ MauiComponents/Dialog.cs
@@ -76,3 +76,12 @@ public interface IDialog
 
     ValueTask Toast(string text, bool longDuration = false, double textSize = 14);
 }
+
+public static class DialogExtensions
+{
+    public static async ValueTask<T?> SelectAsync<T>(this IDialog dialog, IList<T> items, Func<T, string> formatter, int selected = -1, string? title = null, string? cancel = null)
+    {
+        var index = await dialog.SelectAsync(items.Select(formatter).ToArray(), selected, title, cancel).ConfigureAwait(false);
+        return index >= 0 ? items[index] : default;
+    }
+}
