dae3911774ba790ed865c19ae12304fed3e3f034
usausa <machi.pon@gmail.com>
2023-11-21 16:18:39 +0900

    Fix inspection

--- MauiComponents/Speech.SpeechService.cs
+++ MauiComponents/Speech.SpeechService.cs
@@ -56,7 +56,7 @@ public sealed class SpeechService : ISpeechService, IDisposable
 
     public async ValueTask<string?> RecognizeAsync(Progress<string> progress, CultureInfo? culture, CancellationToken cancel)
     {
-        var granted = await speechToText.RequestPermissions(default).ConfigureAwait(true);
+        var granted = await speechToText.RequestPermissions().ConfigureAwait(true);
         if (!granted)
         {
             return null;
