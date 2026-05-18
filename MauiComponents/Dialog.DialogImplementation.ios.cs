afa42c44b87ad95216f84663a467c5f8489523e3
usausa <machi.pon@gmail.com>
2026-05-18 10:54:03 +0900

    Update

--- MauiComponents/Dialog.DialogImplementation.ios.cs
+++ MauiComponents/Dialog.DialogImplementation.ios.cs
@@ -1,19 +1,124 @@
 namespace MauiComponents;
 
-// TODO
+using CoreGraphics;
+
+using UIKit;
+
 public sealed partial class DialogImplementation
 {
-    public partial ValueTask InformationAsync(string message, string? title, string ok) => throw new NotSupportedException();
+#pragma warning disable CA1822
+    public async partial ValueTask InformationAsync(string message, string? title, string ok)
+    {
+        var tcs = new TaskCompletionSource();
+#pragma warning disable CA2000
+        var alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+#pragma warning restore CA2000
+#pragma warning disable CA2000
+        alert.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, _ => tcs.TrySetResult()));
+#pragma warning restore CA2000
+        await PresentAsync(alert).ConfigureAwait(true);
+        await tcs.Task.ConfigureAwait(true);
+    }
+#pragma warning restore CA1822
 
     public partial ValueTask<bool> ConfirmAsync(string message, string? title, string ok, string cancel, bool defaultPositive) => throw new NotSupportedException();
 
     public partial ValueTask<Confirm3Result> Confirm3Async(string message, string? title, string ok, string cancel, string neutral, bool defaultPositive) => throw new NotSupportedException();
 
-    public partial ValueTask<int> SelectAsync(string[] items, int selected, string? title, string? cancel) => throw new NotSupportedException();
+#pragma warning disable CA1822
+    public async partial ValueTask<int> SelectAsync(string[] items, int selected, string? title, string? cancel)
+    {
+        var tcs = new TaskCompletionSource<int>();
+#pragma warning disable CA2000
+        var alert = UIAlertController.Create(title, null, UIAlertControllerStyle.ActionSheet);
+#pragma warning restore CA2000
+        for (var i = 0; i < items.Length; i++)
+        {
+            var index = i;
+#pragma warning disable CA2000
+            alert.AddAction(UIAlertAction.Create(items[i], UIAlertActionStyle.Default, _ => tcs.TrySetResult(index)));
+#pragma warning restore CA2000
+        }
 
-    public partial ValueTask<PromptResult> PromptAsync(string? defaultValue, string? message, string? title, string ok, string cancel, string? placeHolder, PromptParameter? parameter) => throw new NotSupportedException();
+        if (cancel is not null)
+        {
+#pragma warning disable CA2000
+            alert.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, _ => tcs.TrySetResult(-1)));
+#pragma warning restore CA2000
+        }
+
+        await PresentAsync(alert).ConfigureAwait(true);
+        return await tcs.Task.ConfigureAwait(true);
+    }
+#pragma warning restore CA1822
+
+#pragma warning disable CA1822
+    public async partial ValueTask<PromptResult> PromptAsync(string? defaultValue, string? message, string? title, string ok, string cancel, string? placeHolder, PromptParameter? parameter)
+    {
+        var param = parameter ?? PromptParameter.Default;
+        var tcs = new TaskCompletionSource<PromptResult>();
+        UITextField? textField = null;
+
+#pragma warning disable CA2000
+        var alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+#pragma warning restore CA2000
+        alert.AddTextField(field =>
+        {
+            textField = field;
+            field.Text = defaultValue;
+            field.Placeholder = placeHolder;
+            field.KeyboardType = param.PromptType switch
+            {
+                PromptType.Number => param.Sign ? UIKeyboardType.NumbersAndPunctuation : UIKeyboardType.NumberPad,
+                PromptType.Decimal => UIKeyboardType.DecimalPad,
+                _ => UIKeyboardType.Default
+            };
+            if (param.MaxLength > 0)
+            {
+                var maxLength = param.MaxLength;
+                field.ShouldChangeCharacters = (f, range, replacement) =>
+                    (f.Text?.Length ?? 0) - range.Length + replacement.Length <= maxLength;
+            }
+
+            if (!string.IsNullOrEmpty(defaultValue))
+            {
+                field.SelectAll(field);
+            }
+        });
+
+#pragma warning disable CA2000
+        alert.AddAction(UIAlertAction.Create(
+            ok,
+            UIAlertActionStyle.Default,
+            _ => tcs.TrySetResult(new PromptResult(true, textField?.Text ?? string.Empty))));
+#pragma warning restore CA2000
+#pragma warning disable CA2000
+        alert.AddAction(UIAlertAction.Create(
+            cancel,
+            UIAlertActionStyle.Cancel,
+            _ => tcs.TrySetResult(PromptResult.Cancel)));
+#pragma warning restore CA2000
+
+        await PresentAsync(alert).ConfigureAwait(true);
+        return await tcs.Task.ConfigureAwait(true);
+    }
+#pragma warning restore CA1822
 
     public partial IDisposable Indicator() => throw new NotSupportedException();
 
     public partial void Snackbar(string message, int duration, Microsoft.Maui.Graphics.Color? color, Microsoft.Maui.Graphics.Color? textColor) => throw new NotSupportedException();
+
+    private static async Task PresentAsync(UIAlertController alert)
+    {
+        var vc = Platform.GetCurrentUIViewController()!;
+        if (alert.PopoverPresentationController is { } popover)
+        {
+            popover.SourceView = vc.View!;
+            popover.SourceRect = new CGRect(
+                vc.View!.Bounds.GetMidX(), vc.View.Bounds.GetMidY(), 0, 0);
+            popover.PermittedArrowDirections = UIPopoverArrowDirection.Any;
+        }
+
+        await vc.PresentViewControllerAsync(alert, true).ConfigureAwait(true);
+    }
 }
--- MauiComponents/Screen.ScreenImplementation.ios.cs
+++ MauiComponents/Screen.ScreenImplementation.ios.cs
@@ -1,21 +1,59 @@
 namespace MauiComponents;
 
+using Foundation;
+
 using UIKit;
 
-// TODO
 public sealed partial class ScreenImplementation
 {
-#pragma warning disable CA1822
+#pragma warning disable CA2213
+    private NSObject? screenOnObserver;
+    private NSObject? screenOffObserver;
+
+    private FullscreenWrapperViewController? fullscreenWrapper;
+#pragma warning restore CA2213
+
     private partial void PlatformDispose()
     {
+        EnableDetectScreenState(false);
+        fullscreenWrapper?.Dispose();
+        fullscreenWrapper = null;
     }
-#pragma warning restore CA1822
 
     // ------------------------------------------------------------
     // Fullscreen
     // ------------------------------------------------------------
 
-    public partial void SetFullscreen(bool value) => throw new NotSupportedException();
+    public partial void SetFullscreen(bool value)
+    {
+        var window = GetKeyWindow();
+        if (window is null)
+        {
+            return;
+        }
+
+        if (value)
+        {
+            if (fullscreenWrapper is not null)
+            {
+                return;
+            }
+
+            var original = window.RootViewController!;
+            fullscreenWrapper = new FullscreenWrapperViewController(original);
+            window.RootViewController = fullscreenWrapper;
+        }
+        else
+        {
+            if (fullscreenWrapper is null)
+            {
+                return;
+            }
+
+            window.RootViewController = fullscreenWrapper.WrappedViewController;
+            fullscreenWrapper = null;
+        }
+    }
 
     // ------------------------------------------------------------
     // Orientation
@@ -23,7 +61,39 @@ public sealed partial class ScreenImplementation
 
     public partial void SetOrientation(DisplayOrientation orientation) => throw new NotSupportedException();
 
-    public partial void EnableDetectScreenState(bool value) => throw new NotSupportedException();
+    // ------------------------------------------------------------
+    // State
+    // ------------------------------------------------------------
+
+    public partial void EnableDetectScreenState(bool value)
+    {
+        if (value)
+        {
+            if (screenOnObserver is not null)
+            {
+                return;
+            }
+
+            screenOnObserver = NSNotificationCenter.DefaultCenter.AddObserver(
+                UIApplication.DidBecomeActiveNotification,
+                _ => RaiseScreenStateChanged(ScreenStateEventArgs.On));
+            screenOffObserver = NSNotificationCenter.DefaultCenter.AddObserver(
+                UIApplication.DidEnterBackgroundNotification,
+                _ => RaiseScreenStateChanged(ScreenStateEventArgs.Off));
+        }
+        else
+        {
+            if (screenOnObserver is null)
+            {
+                return;
+            }
+
+            NSNotificationCenter.DefaultCenter.RemoveObserver(screenOnObserver);
+            NSNotificationCenter.DefaultCenter.RemoveObserver(screenOffObserver!);
+            screenOnObserver = null;
+            screenOffObserver = null;
+        }
+    }
 
     // ------------------------------------------------------------
     // Brightness
@@ -44,4 +114,39 @@ public sealed partial class ScreenImplementation
     }
 #pragma warning restore CA1822
 #pragma warning restore CA1024
+
+    // ------------------------------------------------------------
+    // Helpers
+    // ------------------------------------------------------------
+
+    private static UIWindow? GetKeyWindow() =>
+        UIApplication.SharedApplication.ConnectedScenes
+            .OfType<UIWindowScene>()
+            .SelectMany(s => s.Windows)
+            .FirstOrDefault(w => w.IsKeyWindow);
+
+    private sealed class FullscreenWrapperViewController : UIViewController
+    {
+        public UIViewController WrappedViewController { get; }
+
+        public FullscreenWrapperViewController(UIViewController wrapped)
+        {
+            WrappedViewController = wrapped;
+        }
+
+        public override bool PrefersStatusBarHidden() => true;
+
+        public override bool PrefersHomeIndicatorAutoHidden => true;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            AddChildViewController(WrappedViewController);
+            View!.AddSubview(WrappedViewController.View!);
+            WrappedViewController.View!.Frame = View.Bounds;
+            WrappedViewController.View.AutoresizingMask =
+                UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+            WrappedViewController.DidMoveToParentViewController(this);
+        }
+    }
 }
