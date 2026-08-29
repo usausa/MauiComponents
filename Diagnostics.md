6a886f5cef3aae4962e0abb730eb4aec38388d66
usausa <machi.pon@gmail.com>
2026-08-29 09:30:52 +0900

    Update

--- Diagnostics.md
+++ Diagnostics.md
@@ -2,6 +2,6 @@
 
 | ID | Severity | Description | How to fix |
 |---|---|---|---|
-| MC0001 | ❌ Error | Target method is not a partial extension method | Declare the method as a `partial` extension method |
-| MC0002 | ❌ Error | Target method has parameters | Remove the parameters from the method |
-| MC0003 | ❌ Error | Target method does not return `IEnumerable<KeyValuePair<ViewId, Type>>` | Change the return type to `IEnumerable<KeyValuePair<ViewId, Type>>` |
+| MC0001 | ❌ Error | `[PopupSource]` method is not a partial extension method | Declare the method as a `partial` extension method |
+| MC0002 | ❌ Error | `[PopupSource]` method has parameters | Remove the parameters from the method |
+| MC0003 | ❌ Error | `[PopupSource]` method does not return `IEnumerable<KeyValuePair<ViewId, Type>>` | Change the return type to `IEnumerable<KeyValuePair<ViewId, Type>>` |
--- MauiComponents.Generator/Diagnostics.cs
+++ MauiComponents.Generator/Diagnostics.cs
@@ -7,7 +7,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodDefinition { get; } = new(
         id: "MC0001",
         title: "Invalid method definition",
-        messageFormat: "Method must be partial extension. method=[{0}]",
+        messageFormat: "[PopupSource] method must be partial extension. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -15,7 +15,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodParameter { get; } = new(
         id: "MC0002",
         title: "Invalid method parameter",
-        messageFormat: "Method must not have parameters. method=[{0}]",
+        messageFormat: "[PopupSource] method must not have parameters. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -23,7 +23,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodReturnType { get; } = new(
         id: "MC0003",
         title: "Invalid method return type",
-        messageFormat: "Return type must be IEnumerable<KeyValuePair<ViewId, Type>>. method=[{0}]",
+        messageFormat: "[PopupSource] return type must be IEnumerable<KeyValuePair<ViewId, Type>>. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
