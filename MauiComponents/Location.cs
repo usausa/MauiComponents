1e032f3f57137ebad7b46b9ea72a8bcbbfe9cc9e
usausa <machi.pon@gmail.com>
2026-09-01 11:09:17 +0900

    Update

--- MauiComponents/Location.cs
+++ MauiComponents/Location.cs
@@ -12,7 +12,6 @@ public sealed class LocationEventArgs : EventArgs
     }
 }
 
-#pragma warning disable CA1716
 public interface ILocationService
 {
     event EventHandler<LocationEventArgs>? LocationChanged;
@@ -27,4 +26,3 @@ public interface ILocationService
 
     ValueTask<Location?> GetLocationAsync(GeolocationAccuracy accuracy = GeolocationAccuracy.Medium, int timeout = 15000, CancellationToken cancel = default);
 }
-#pragma warning restore CA1716
